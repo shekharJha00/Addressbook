cf3e3e309cf2986e6531c6b5805781cc98a239fd
shekhar <shekharjha7417870578@gmail.com>
2022-08-17 15:25:49 +0530

    Added Uc6 : Reafactor code for multiple adress books

---
+++ src/com/bridgelabz/adressbook/ContactsDetails.java
@@ -0,0 +1,80 @@
+package com.bridgelabz.adressbook;
+
+public class ContactsDetails {
+
+    private String firstname, lastname, address, city, state, email;
+    private int zip;
+    private long phonenumber;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(long phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactsDetails [firstname=" + firstname + ", lastname=" + lastname + ", address=" + address + ", city="
+                + city + ", state=" + state + ", email=" + email + ", zip=" + zip + ", phonenumber=" + phonenumber
+                + "]";
+    }
+
+}
