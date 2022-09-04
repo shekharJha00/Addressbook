b2df436452f346c9ec4337be6fa5f43c8791d0a1
shekharJha00 <99266926+shekharJha00@users.noreply.github.com>
2022-09-04 21:46:02 +0530

    Update ContactsDetails.java

--- src/com/bridgelabz/adressbook/ContactsDetails.java
+++ src/com/bridgelabz/adressbook/ContactsDetails.java
@@ -2,7 +2,7 @@ package com.bridgelabz.adressbook;
 
 
 public class ContactsDetails {
-    private String first_name;
+     private String first_name;
     private String last_name;
     private String phone_number;
     private String email;
@@ -45,42 +45,11 @@ public class ContactsDetails {
 
     public void setEmail(String email) {
         this.email = email;
-
-public class ContactsDetails {
-
-    private String firstname, lastname, address, city, state, email;
-    private int zip;
-    private long phonenumber;
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-
     }
 
     public String getCity() {
         return city;
     }
-
     public void setCity(String city) {
         this.city = city;
     }
@@ -88,59 +57,21 @@ public class ContactsDetails {
     public String getState() {
         return state;
     }
-
     public void setState(String state) {
         this.state = state;
     }
-
 
     public String getZipcode() {
         return zipcode;
     }
 
-
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
-    public long getPhonenumber() {
-        return phonenumber;
-    }
-
-    public void setPhonenumber(long phonenumber) {
-        this.phonenumber = phonenumber;
-
     }
 
     @Override
     public String toString() {
-
         return "First Name : '" + first_name + "', Last Name : '" + last_name + "', Phone Number : '" + phone_number +
                 "', email ID : '" + email + "', City : '" + city + "', State : '" + state + "', Zipcode : '" + zipcode + "'\n";
-
     }}
-    }
-}
-
-        return "ContactsDetails [firstname=" + firstname + ", lastname=" + lastname + ", address=" + address + ", city="
-                + city + ", state=" + state + ", email=" + email + ", zip=" + zip + ", phonenumber=" + phonenumber
-                + "]";
-    }
-
-}
 
